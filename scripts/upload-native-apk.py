478a192487fdab89035c7e41699edc8f532abdde
Z User <z@container>
2026-07-08 03:26:14 +0000

    f2f662f3-623d-4961-912f-3ad2911a6a1c

--- scripts/upload-native-apk.py
+++ scripts/upload-native-apk.py
@@ -8,12 +8,12 @@ token = result.stdout.strip().split(':', 2)[2].split('@')[0]
 
 api = HfApi(token=token)
 repo_id = 'safwatkhokha/nawaqes-backup'
-APK_VERSION = 'v3.0.1'
+APK_VERSION = 'v3.0.2'
 apk_path = f'/home/z/my-project/download/nawaqes-native-{APK_VERSION}.apk'
 path_in_repo = f'nawaqes-native-{APK_VERSION}.apk'
 
 print(f'[UPLOAD] {apk_path} ({os.path.getsize(apk_path)/1024/1024:.1f} MB)')
 api.upload_file(path_or_fileobj=apk_path, path_in_repo=path_in_repo,
     repo_id=repo_id, repo_type='dataset',
-    commit_message=f'Upload Nawaqes Native {APK_VERSION} APK (fix splash logo stuck)')
+    commit_message=f'Upload Nawaqes Native {APK_VERSION} APK (fix RNSVGRect error)')
 print(f'[UPLOAD] Done! URL: https://huggingface.co/datasets/{repo_id}/resolve/main/{path_in_repo}')
